aebffc574bf615ea8784e89e7071784ad0446125
Julissa <Julissa@JULS>
2024-07-31 21:07:09 -0600

    Agregar archivos de proyecto.

---
+++ SumaDEDOSNUMEROS/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SumaDEDOSNUMEROS
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
